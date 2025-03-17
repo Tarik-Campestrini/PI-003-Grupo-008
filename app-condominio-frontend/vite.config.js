4c815376ec2397b966e26b9fff0bf98c820abcc8
TarikCampestrini <tarikrc@hotmail.com>
2025-03-17 10:03:56 -0300

    Rotas backend

--- app-condominio-frontend/vite.config.js
+++ app-condominio-frontend/vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000/",
+        target: "https://app-comdominio-backend.vercel.app/",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, "/api"),
